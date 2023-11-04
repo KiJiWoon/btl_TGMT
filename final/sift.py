78375d6e5ed96d91c1cf1579883d504855f4c533
KiJiWoon <117337012+KiJiWoon@users.noreply.github.com>
2023-11-04 07:44:06 +0700

    Add files via upload

--- final/sift.py
+++ final/sift.py
@@ -4,7 +4,9 @@ import cv2
 import matplotlib.pyplot as plt 
 import pickle
 import sklearn 
+import time
 
+from cvzone.HandTrackingModule import HandDetector
 from sklearn.cluster import KMeans
 from scipy.spatial.distance import cdist
 from sklearn.model_selection import train_test_split 
@@ -21,7 +23,7 @@ def read_data(label2id):
     return X,Y
 
 #Gan nhan du lieu
-label2id = {'peace':0, 'hello':1, 'thump':2, 'fist':3, 'right_hand':4}
+label2id = {'peace':0, 'hello':1, 'thump':2, 'fist':3, 'ok':4}
 X, Y = read_data(label2id)
 
 #Trich xuat dac trung
@@ -86,3 +88,54 @@ svm = sklearn.svm.SVC(C = 10)
 svm.fit(x_train,y_train)
 
 print(svm.score(x_test, y_test))
+
+x = [300, 245, 200, 170, 145, 130, 112, 103, 93, 87, 80, 75, 70, 67, 62, 59, 57]
+y = [20, 25, 30, 40, 45, 50, 60, 65, 70, 75, 80, 85, 90, 95, 100]
+
+cap = cv2.VideoCapture(0)
+detector = HandDetector(detectionCon=0.9,maxHands=1)
+
+while True:
+    ret, frame = cap.read()
+    hands, frame = detector.findHands(frame, draw=False)
+
+    numeric = 0
+
+    if hands:
+
+        x,y,w,h = hands[0]['bbox']
+
+        cv2.imwrite('./valid/hand_gesture_{}.jpg'.format(numeric),frame[y-20: (y+h)+35, x-20: (x+w)+35])
+        # cv.imwrite('./hand_cut/hand_gesture.jpg',frame[y-20: (y+h)+35, x-20: (x+w)+35])
+            
+
+        cv2.rectangle(frame, (x-20,y-20), ( (x+w) + 35 , (y+h) + 35 ), (0,255,0), 3)
+
+        numeric = numeric + 1
+
+    if ret == True:
+        
+        file = 'valid'
+        for i in os.listdir(file):
+            detect = os.path.join(file, i)
+            img_test = cv2.imread(detect)
+            img = [img_test]
+            img_sift_feature = extr_sift_feature(img)
+            img_bow_feature = create_features_bow(img_sift_feature,BOW,num_cluster)
+            img_predict = svm.predict(img_bow_feature)
+            print(img_predict)
+        
+
+            for key, value in label2id.items():
+                if value == img_predict[0]:
+                    cv2.putText(frame,key, (50,50), cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),1)
+        cv2.imshow("result", frame)
+
+        
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break 
+
+
+vid.release()
+cv2.destroyAllWidows()
